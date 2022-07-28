ea1f8c75da8482534bb4971836cd5bd10fabe193
Glitch (glitch-hello-website) <none>
2022-07-28 18:20:17 +0000

    📮🌟 Checkpoint ./backgroundmusic.js:26040239/1192

---
+++ backgroundmusic.js
@@ -0,0 +1,9 @@
+<html>
+<body>
+<audio autoplay>
+  <source
+src: url="https://cdn.glitch.global/180b8256-cef5-431d-834f-d445798120cf/index.mp3?v=1659025211896" type="audio/mp3">
+<audio loop>
+</audio>
+</body>
+</html>
